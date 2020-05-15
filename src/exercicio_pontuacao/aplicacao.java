f89133b83b5812a9aaeac8046cec08d3e1acb14e
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-15 20:20:13 -0300

    Score de volume de compras

--- src/exercicio_pontuacao/aplicacao.java
+++ src/exercicio_pontuacao/aplicacao.java
@@ -17,18 +17,29 @@ public class aplicacao {
 		System.out.print("Quantas compras o cliente fez no último ano? ");
 		int quantCompras =sc.nextInt();
 		System.out.print("Qual o ticket médio? ");
-		double tiket = sc.nextDouble();
+		double ticket = sc.nextDouble();
 		System.out.println();
 		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
 		int atraso = sc.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		char formaPag= sc.next().toUpperCase().charAt(0);
-
+		System.out.println();
+		System.out.println("Score de volume de compras = 40 pontos");
 		
 		
-		
-		
-
+		int scoreCompras = 0;
+		double numQuantMedioCompra = ticket * quantCompras;
+		if (quantCompras == 0 && numQuantMedioCompra == 0) {
+			scoreCompras = 0;
+		} else if (numQuantMedioCompra <= 3000 && quantCompras <= 2) {
+			scoreCompras += 20;
+		} else if (quantCompras > 2 && numQuantMedioCompra <= 3000) {
+			scoreCompras += 40;
+		} else if (numQuantMedioCompra > 3000) {
+			scoreCompras += 60;
+		}
+		System.out.println("Score de volume de compras = " + scoreCompras);
+		System.out.println();
 	}
 
 }
