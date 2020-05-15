8bc6ce51f52335b192c8d051ae1976e2ba3d40d9
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-15 19:31:44 -0300

    Cabecalho

--- src/exercicio_pontuacao/aplicacao.java
+++ src/exercicio_pontuacao/aplicacao.java
@@ -10,6 +10,18 @@ public class aplicacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("----------------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		System.out.println("Quantas compras o cliente fez no último ano?");
+		System.out.println("Qual o ticket médio?");
+		System.out.println();
+		System.out.println("Quantas vezes o cliente atrasou o pagamento?");
+		System.out.println("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)?");
+		
+		
+		
 		
 
 	}
